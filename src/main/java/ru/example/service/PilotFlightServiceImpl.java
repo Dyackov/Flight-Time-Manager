5af3f44ba58d0c80c18b5e55bfc27b027d1e9b9b
Dyackov <dyackov.sergei@uandex.ru>
2025-02-01 16:33:53 +0300

    Добавлено логирование Slf4j

--- src/main/java/ru/example/service/PilotFlightServiceImpl.java
+++ src/main/java/ru/example/service/PilotFlightServiceImpl.java
@@ -22,15 +22,37 @@ public class PilotFlightServiceImpl implements PilotFlightService {
     private final PilotValidator pilotValidator = new PilotValidator();
 
     public OutputPilotsAndFlights process(InputPilotsAndFlights inputPilotsAndFlights) {
+        log.info("Обработка данных InputPilotsAndFlights");
         OutputPilotsAndFlights outputPilotsAndFlights = new OutputPilotsAndFlights();
 
-        // Распределение в маппу
-        savedPilotAndFlights(inputPilotsAndFlights);
+        try {
+            log.debug("Начало сохранения данных пилотов и рейсов...");
+            savedPilotAndFlights(inputPilotsAndFlights);
+            log.info("Данные пилотов и рейсов успешно сохранены.");
 
-        // сохранение в класс Specialist
-        savedOutPut(outputPilotsAndFlights);
+            log.debug("Начало сохранения данных в Specialist...");
+            savedOutPut(outputPilotsAndFlights);
+            log.info("Данные в Specialist успешно сохранены.");
 
+        } catch (Exception e) {
+            log.error("Ошибка при обработке InputPilotsAndFlights", e);
+        }
+        log.info("Обработка данных InputPilotsAndFlights завершена");
         return outputPilotsAndFlights;
+    }
+
+    public void savedOutPut(OutputPilotsAndFlights outputPilotsAndFlights) {
+        log.info("Начало обработки данных для сохранения в outputPilotsAndFlights");
+        for (Map.Entry<Pilot, List<Flight>> entry : flightsByPilot.entrySet()) {
+            Pilot pilot = entry.getKey();
+            List<Flight> flights = entry.getValue();
+
+            // Рассчитываем данные по налету
+            List<TimeMonth> timeMonths = calculateFlightTime(flights);
+            pilot.setTimeMonthList(timeMonths);
+            outputPilotsAndFlights.getSpecialists().add(pilot);
+        }
+        log.info("Завершена обработка данных и сохранение в outputPilotsAndFlights");
     }
 
     public void savedPilotAndFlights(InputPilotsAndFlights inputPilotsAndFlights) {
@@ -55,7 +77,6 @@ public class PilotFlightServiceImpl implements PilotFlightService {
                 flightValidator.validate(flight); // Проверка валидности рейса
                 log.info("Рейс с ID {} прошёл валидацию", flight.getId());
             } catch (IllegalArgumentException e) {
-                // Логируем ошибку валидации рейса
                 log.error("Ошибка валидации рейса с ID {}: {}", flight.getId(), e.getMessage());
                 validationErrors.add("Ошибка валидации рейса " + flight.getId() + ": " + e.getMessage());
                 isValidFlight = false;
@@ -71,25 +92,9 @@ public class PilotFlightServiceImpl implements PilotFlightService {
                 });
             }
         });
-        // Логируем все ошибки валидации пилотов в консоль
         if (!validationErrors.isEmpty()) {
             System.err.println("Обнаружены ошибки валидации:");
             validationErrors.forEach(System.err::println);
-        }
-    }
-
-    public void savedOutPut(OutputPilotsAndFlights outputPilotsAndFlights) {
-        for (Map.Entry<Pilot, List<Flight>> entry : flightsByPilot.entrySet()) {
-            // конкретный пилот
-            Pilot pilot = entry.getKey();
-            // полёты конкретного пилота
-            List<Flight> flights = entry.getValue();
-
-            // Рассчитываем данные по налету
-            List<TimeMonth> timeMonths = calculateFlightTime(flights);
-
-            pilot.setTimeMonthList(timeMonths);
-            outputPilotsAndFlights.getSpecialists().add(pilot);
         }
     }
 
@@ -97,19 +102,21 @@ public class PilotFlightServiceImpl implements PilotFlightService {
     // Расчёта общего времени полёта за месяц с флагами
     private List<TimeMonth> calculateFlightTime(List<Flight> flights) {
         List<TimeMonth> timeMonths = new ArrayList<>();
-        // 1. Рассчитываем налет за день
+        log.info("Начало расчета общего времени полетов за месяц");
+
         Map<LocalDate, Long> hoursPerDay = getHoursPerDay(flights);
+        log.debug("Налет по дням: {}", hoursPerDay);
 
-        // 2. Рассчитываем налет за неделю
         Map<LocalDate, Long> hoursPerWeek = getHoursPerWeek(hoursPerDay);
+        log.debug("Налет по неделям: {}", hoursPerWeek);
 
-        // 3. Рассчитываем налет за месяц
         Map<LocalDate, Long> hoursPerMonth = getHoursPerMonths(hoursPerDay);
+        log.debug("Налет по месяцам: {}", hoursPerMonth);
 
-        // 4. Рассчитываем количество полетов за месяц
         Map<LocalDate, Long> totalFlightsPerMonth = flights.stream()
                 .collect(Collectors.groupingBy(flight -> flight.getDepartureTime().toLocalDate().withDayOfMonth(1),
                         Collectors.counting()));
+        log.debug("Количество полетов по месяцам: {}", totalFlightsPerMonth);
 
         for (Map.Entry<LocalDate, Long> monthEntry : hoursPerMonth.entrySet()) {
             LocalDate month = monthEntry.getKey();
@@ -121,14 +128,16 @@ public class PilotFlightServiceImpl implements PilotFlightService {
             // Флаг превышения месячного лимита
             boolean exceedsMonthlyLimit = totalMonthHours > 80;
 
-            // Создаем объект TimeMonth
             TimeMonth timeMonth = new TimeMonth();
             timeMonth.setDate(month);
             timeMonth.setTotalFlightsInMonth(totalFlightsInMonth);
             timeMonth.setTotalFlightHours(totalMonthHours);
             timeMonth.setExceedsMonthlyLimit(exceedsMonthlyLimit);
 
-            // 5. Проверяем недельный лимит (в рамках этого месяца)
+            log.debug("Время полетов за месяц {}: {} часов, количество полетов: {}, превышен месячный лимит: {}",
+                    month, totalMonthHours, totalFlightsInMonth, exceedsMonthlyLimit);
+
+            // Проверяем недельный лимит (в рамках этого месяца)
             long maxWeeklyHours = hoursPerWeek.entrySet().stream()
                     .filter(entry -> {
                         LocalDate currentWeekDate = entry.getKey();
@@ -145,29 +154,32 @@ public class PilotFlightServiceImpl implements PilotFlightService {
                     .orElse(0);
 
             timeMonth.setExceedsWeeklyLimit(maxWeeklyHours > 36);
+            log.debug("Максимальное количество часов в неделю для месяца {}: {} часов, превышен недельный лимит: {}",
+                    month, maxWeeklyHours, maxWeeklyHours > 36);
 
-            // 6. Проверяем дневной лимит (в рамках этого месяца)
+            // Проверяем дневной лимит (в рамках этого месяца)
             long maxDayHoursForMonth = hoursPerDay.entrySet().stream()
                     .filter(entry -> entry.getKey().getMonth().equals(month.getMonth()))
                     .mapToLong(Map.Entry::getValue)
                     .max()
                     .orElse(0);
             timeMonth.setExceedsDailyLimit(maxDayHoursForMonth > 8);
+            log.debug("Максимальное количество часов в день для месяца {}: {} часов, превышен дневной лимит: {}",
+                    month, maxDayHoursForMonth, maxDayHoursForMonth > 8);
 
             timeMonths.add(timeMonth);
         }
+        log.info("Завершен расчет общего времени полетов за месяц");
         return timeMonths;
     }
 
     //TODO original
     // Возвращает количество часов полёта за 1 день
     private Map<LocalDate, Long> getHoursPerDay(List<Flight> flights) {
-        // 1 день
         Map<LocalDate, Long> sumHoursPerDay = new TreeMap<>();
+        log.info("Начало расчета количества часов полета за 1 день");
         for (Flight flight : flights) {
-            // Дата вылета
             LocalDateTime departureTime = flight.getDepartureTime();
-            // Дата прилёта
             LocalDateTime arrivalTime = flight.getArrivalTime();
 
             try {
@@ -176,6 +188,8 @@ public class PilotFlightServiceImpl implements PilotFlightService {
                 log.warn("Рейс {} пропущен: {}", flight.getId(), e.getMessage());
                 continue; // Пропускаем этот рейс
             }
+
+            log.debug("Обрабатываем рейс с ID: {} от {} до {}", flight.getId(), departureTime, arrivalTime);
 
             while (departureTime.isBefore(arrivalTime)) {
                 // День вылета
@@ -188,15 +202,18 @@ public class PilotFlightServiceImpl implements PilotFlightService {
                 if (departureDay.equals(arrivalDay)) {
                     long durationInHoursDay = Duration.between(departureTime, arrivalTime).toHours();
                     sumHoursPerDay.merge(departureDay, durationInHoursDay, Long::sum);
+                    log.debug("Время полета для дня {}: {} часов", departureDay, durationInHoursDay);
                     departureTime = nextDay;
                 } else {
                     long durationInHours = Duration.between(departureTime, nextDay).toHours();
                     sumHoursPerDay.merge(departureDay, durationInHours, Long::sum);
+                    log.debug("Часы полета для дня {}: {} часов", departureDay, durationInHours);
                     // Обновляем начало на первый день следующего месяца
                     departureTime = nextDay;
                 }
             }
         }
+        log.info("Завершен расчет количества часов полета за 1 день");
         return sumHoursPerDay;
     }
 
@@ -204,22 +221,21 @@ public class PilotFlightServiceImpl implements PilotFlightService {
     // Возвращает количество часов полёта за 7 дней
     private Map<LocalDate, Long> getHoursPerWeek(Map<LocalDate, Long> hoursPerDay) {
         Map<LocalDate, Long> sumHoursPerWeek = new TreeMap<>();
-
+        log.info("Начало расчета количества часов полета за неделю");
         for (Map.Entry<LocalDate, Long> hoursPerWeek : hoursPerDay.entrySet()) {
-            // конкретная день
+            // Конкретный день
             LocalDate weekday = hoursPerWeek.getKey();
 
-            // кол-во часов полёта в день
+            // Количество часов полета в день
             Long hours = hoursPerWeek.getValue();
 
-            // начало недели исходя из дня
+            // Рассчитываем начало недели (понедельник)
             LocalDate startOfWeek = weekday.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
 
-            // конец недели исходя из дня
-            //           LocalDate endOfWeek = weekday.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
-
             sumHoursPerWeek.merge(startOfWeek, hours, Long::sum);
+            log.debug("Дата: {} | Часы: {} | Начало недели: {}", weekday, hours, startOfWeek);
         }
+        log.info("Завершен расчет количества часов полета за неделю");
         return sumHoursPerWeek;
     }
 
@@ -227,13 +243,17 @@ public class PilotFlightServiceImpl implements PilotFlightService {
     // Возвращает количество часов полёта за месяц
     private Map<LocalDate, Long> getHoursPerMonths(Map<LocalDate, Long> hoursPerDay) {
         Map<LocalDate, Long> sumHoursPerMonth = new TreeMap<>();
-
+        log.info("Начало расчета количества часов полета за месяц");
         for (Map.Entry<LocalDate, Long> hoursPerMonth : hoursPerDay.entrySet()) {
             LocalDate month = hoursPerMonth.getKey();
             Long hours = hoursPerMonth.getValue();
+            // Рассчитываем начало месяца
             LocalDate startOfMonth = month.with(TemporalAdjusters.firstDayOfMonth());
+            // Добавляем количество часов в карту по началу месяца
             sumHoursPerMonth.merge(startOfMonth, hours, Long::sum);
+            log.debug("Дата: {} | Часы: {} | Начало месяца: {}", month, hours, startOfMonth);
         }
+        log.info("Завершен расчет количества часов полета за месяц");
         return sumHoursPerMonth;
     }
 }
